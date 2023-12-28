6a80575e00cb6ada97f1812ae233536488d501ca
Maximiliano F Romero <maximilianoromerovigo@gmail.com>
2023-12-28 10:25:17 -0300

    docs(swagger): Swagger socumentation added

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,14 @@ async function bootstrap() {
       forbidNonWhitelisted: true
     })
   )
+  const config = new DocumentBuilder()
+    .setTitle('Car Dealership')
+    .setDescription('Car dealership API')
+    .setVersion('0.5')
+    .addTag('CarDealership')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT);
   console.log(`App running on port ${process.env.PORT}`);
 }
